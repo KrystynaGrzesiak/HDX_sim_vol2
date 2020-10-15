91242ac52fb1a03f95bd2022280dcf4245c567f8
KrystynaGrzesiak <krygrz11@gmail.com>
2020-10-15 13:56:12 +0200

    changes sim time

---
+++ theo_spectra_sim/do_missing_sim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --output=missing.out
+#SBATCH --job-name="missing_sim"
+#
+#SBATCH --time=30:00:00
+
+module purge
+
+module load system/R−3.6.1
+
+~/R-3.6.1/bin/Rscript missing_sim.R
