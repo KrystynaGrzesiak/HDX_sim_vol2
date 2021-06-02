83a4885529f120adec1a290cfb13d063478d5f5a
KrystynaGrzesiak <krygrz11@gmail.com>
2021-06-02 16:18:51 +0200

    adds lasso and ridge

--- power_sim/regularization/regularization.R
+++ power_sim/regularization/regularization.R
@@ -7,82 +7,72 @@ truncated_lines <- function(x, kappa){
 }
 
 
-### ridge
-
-S_reg <- function(data, significance_level = 0.05) {
+S_lasso_ridge <- function(data, significance_level = 0.05) {
   
   States = unique(data$State)
-  
-  aic = loglik = Test_statistic = p_value = rep(NA, 2)
-  data[["Exposure"]] = data[["Exposure"]]
+  data[["id"]] <- paste0(data$Rep, data$Charge, data$Experimental_state)
+  Test = aic = loglik = Test_statistic = p_value = rep(NA, 2)
   
   knots <- unique(setdiff(data$Exposure, c(max(data$Exposure), min(data$Exposure))))
-  
-  X <- sapply(1:length(knots), function(knot) {
-    truncated_lines(data$Exposure, knots[knot])
+  X <- sapply(knots, function(k) {
+    truncated_lines(data$Exposure, k)
   })
-  
-  design_matrix <- cbind(data[["Exposure"]],
-                         data[["Experimental_state"]] == "A", 
-                         X, 
-                         data[["Exposure"]]*(data[["Experimental_state"]] == "A"))
-  
-  colnames(design_matrix) <- c("Exposure", "State", as.character(knots), "Exposure_State")
-  
-  cv_fit <- cv.glmnet(design_matrix, data[["Mass"]], alpha = 1)
-  
-  m1 <- glmnet(design_matrix, data[["Mass"]], 
-               alpha = 0,
-               lambda = 0.005)
-  
-  m2 <- glmnet(design_matrix[, -c(2, 22)], data[["Mass"]], 
-               alpha = 0,
-               lambda = 0.005)
-  
-  anova(m1, m2)
+  colnames(X) <- c(paste0("knot_", as.character(knots)))
   
   
-  res <- summary(cv_fit)
-  coef(cv_fit)
+  #LASSO
   
-  summary(m1)
+  cv_fit <- glmnet(X, data[["Mass"]], alpha = 1, lambda = 0.001)
+  coefs <- coefficients(cv_fit)
+  X_reduced <- cbind(intercept = 1, X)[, which(coefs != 0)]
+  
+  # continuous, identity
+  model = lmerTest::lmer(Mass ~ Exposure*Experimental_state + (1|id) + X_reduced,
+                         data = data,
+                         REML = FALSE)
+  model_reduced = lmerTest::lmer(Mass ~ Exposure + (1|id) + X_reduced,
+                                 data = data,
+                                 REML = FALSE)
+  result = anova(model, model_reduced)
+  aic[1] = AIC(model)
+  loglik[1] = as.numeric(logLik(model))
+  Test_statistic[1] = result$Chisq[2]
+  p_value[1] = result$`Pr(>Chisq)`[2]
+  
+  Test[1] = "LASSO_knots_random_intercept_id"
   
   
+  #RIDGE
+  
+  cv_fit <- glmnet(X, data[["Mass"]], alpha = 0, lambda = 0.001)
+  coefs <- coefficients(cv_fit)
+  X_reduced <- cbind(intercept = 1, X)[, which(abs(coefs) >= 2*10^(-5))]
+  
+  # continuous, identity
+  model = lmerTest::lmer(Mass ~ Exposure*Experimental_state + (1|id) + X_reduced,
+                         data = data,
+                         REML = FALSE)
+  model_reduced = lmerTest::lmer(Mass ~ Exposure + (1|id) + X_reduced,
+                                 data = data,
+                                 REML = FALSE)
   result = anova(model, model_reduced)
   aic[2] = AIC(model)
-  loglik[2] = logLik(model)
-  Test_statistic[2] = result$`F`[2]
-  p_value[2] = result$`Pr(>F)`[2]
+  loglik[2] = as.numeric(logLik(model))
+  Test_statistic[2] = result$Chisq[2]
+  p_value[2] = result$`Pr(>Chisq)`[2]
   
-  data.table::data.table(Test = "S1",
+  Test[2] = "RIDGE_knots_random_intercept_id"
+  
+  data.table::data.table(Test = Test,
                          State_1 = States[1],
                          State_2 = States[2],
                          Test_statistic = Test_statistic,
                          P_value = p_value,
                          Significant_difference = (p_value <= significance_level),
                          Time = NA,
-                         Transformation = Transformation,
+                         Transformation = "identity",
                          AIC = aic,
                          logLik = loglik)
 }
-
-
-### lasso
-
-S_lasso <- function(data, significance_level = 0.05) {
-  
-}
-
-
-
-### slope
-
-
-
-S_slope <- function(data, significance_level = 0.05) {
-  
-}
-
-
 
 
